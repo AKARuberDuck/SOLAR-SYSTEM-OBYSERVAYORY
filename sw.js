a533e6077f0fe114a34113c05ec2b179de231071
AKARuberDuck <ss2875356@gmail.com>
2025-06-23 17:24:20 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "solar-system-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./favicon.png",
+  "./8k_stars_milky_way.jpg",
+  "./splash-loading-screen.png",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request);
+    })
+  );
+});
